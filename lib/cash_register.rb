d09f5afcc1b062ece5e85fcb1b0572863e875101
Lex Nelson <lexinelson597@gmail.com>
2021-08-11 16:58:01 -0400

    commit tree 7ed1975a04e05176d20a55cef6448bef759d65a6

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+    attr_accessor :total, :discount
+    
 
+    def initialize( discount=0)
+        @discount=discount
+        @total = 0
+        
+    end
+    def total
+        @total
+    end
+    def add_item(title, price, qty=1)
+        @total += (price * qty)
+        @total
+    end
+    def apply_discount
+        percent= discount.to_f 
+        percent * 0.01
+        newT= @total
+        if discount >0 
+            @total - (newT * percent)
+        end
+    end
+            
+end
